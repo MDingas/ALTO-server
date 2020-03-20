0b0a341f405cd7aa03cebef05b379ed99a63e734
Paulo Caldas <pauloedgar2@gmail.com>
2020-03-20 16:07:28 +0000

    Revert "Base commit"
    This reverts commit 2e773df3327b94350812a3cc9796687c0c241f07.

--- src/main/java/com/example/restservice/entities/interfaces/MultiCostMap.java
+++
@@ -1,8 +0,0 @@
-package com.example.restservice.entities.interfaces;
-
-public interface MultiCostMap {
-    String getCost(String metric, PID src, PID dst);
-    void setCost(String metric, PID src, PID dst);
-    void clear(String metric);
-    void clear();
-}
--- src/main/java/com/example/restservice/repository/NetworkMapRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.restservice.repository;
-
-import com.example.restservice.dto.NetworkMapDTO;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NetworkMapRepository extends MongoRepository<NetworkMapDTO, String> {
-}
