3bc6c0c790f88d2053e120b6e390832fa4fb6e9b
Paulo Caldas <pauloedgar2@gmail.com>
2020-03-30 10:55:50 +0100

    Added enum representations of enum-able fields in the cost map resource

---
+++ src/main/java/com/example/restservice/dto/costmap/CostMetricDTO.java
@@ -0,0 +1,52 @@
+package com.example.restservice.dto.costmap;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Identifies what a cost represents
+ *
+ * Further defined in https://www.rfc-editor.org/rfc/rfc7285.txt chapter 6.1.1
+ * and https://www.ietf.org/id/draft-ietf-alto-performance-metrics-09.txt
+ */
+
+public enum CostMetricDTO {
+    GENERIC_ROUTING_COST("routingcost"),
+    ONE_WAY_DELAY("owdelay"),
+    ROUND_TRIP_DELAY("rtt"),
+    PACKET_DELAY_VARIATION("pdv"),
+    HOP_COUNT("hopcount"),
+    PACKET_LOSS("pktloss"),
+    THROUGHPUT("throughput"),
+    MAX_RESERVABLE_BANDWIDTH("maxresbw"),
+    RESIDUE_BANDWIDTH("residuebw");
+
+    private final String value;
+    private static final Map<String, CostMetricDTO> ENUM_MAP;
+
+    static {
+        Map<String, CostMetricDTO> valueToEnumMap = new HashMap<>();
+        for (CostMetricDTO costMetricDTO : CostMetricDTO.values()) {
+            valueToEnumMap.put(costMetricDTO.toString(), costMetricDTO);
+        }
+        ENUM_MAP = Collections.unmodifiableMap(valueToEnumMap);
+    }
+
+    private CostMetricDTO(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static CostMetricDTO fromString(String value) {
+        return ENUM_MAP.get(value);
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/com/example/restservice/dto/costmap/CostModeDTO.java
@@ -0,0 +1,31 @@
+package com.example.restservice.dto.costmap;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * How to interpret the values of a given cost map
+ *
+ * Further defined in https://www.rfc-editor.org/rfc/rfc7285.txt chapter 6.1.2
+ */
+public enum CostModeDTO {
+    NUMERICAL("numerical"),
+    ORDINAL("ordinal"),
+    NOMINAL("nominal");
+
+    private final String value;
+
+    private CostModeDTO(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static CostModeDTO fromString(String value) {
+        return value == null ? null : CostModeDTO.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+}
