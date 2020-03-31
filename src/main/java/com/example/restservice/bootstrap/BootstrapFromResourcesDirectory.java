676bf4bd2f40600272f078794929f15807c48e0d
Paulo Caldas <pauloedgar2@gmail.com>
2020-03-31 12:30:51 +0100

    Formatting. Now using objectmapper that throws exception if java bean validations fail atleast once

--- src/main/java/com/example/restservice/bootstrap/BootstrapFromResourcesDirectory.java
+++ src/main/java/com/example/restservice/bootstrap/BootstrapFromResourcesDirectory.java
@@ -1,10 +1,21 @@
 package com.example.restservice.bootstrap;
 
+import com.example.restservice.dto.calendarcostmap.CalendarCostMapDTO;
 import com.example.restservice.dto.costmap.CostMapDTO;
+import com.example.restservice.dto.informationresourcedirectory.InformationResourceDirectoryDTO;
 import com.example.restservice.dto.networkmap.NetworkMapDTO;
+import com.example.restservice.repository.CalendarCostMapRepository;
 import com.example.restservice.repository.CostMapRepository;
+import com.example.restservice.repository.InformationResourceDirectoryRepository;
 import com.example.restservice.repository.NetworkMapRepository;
+import com.example.restservice.validation.BeanValidationDeserializer;
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.DeserializationConfig;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.BeanDeserializer;
+import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,38 +30,89 @@ import java.util.List;
 @Component
 public class BootstrapFromResourcesDirectory implements CommandLineRunner {
     private static final String RESOURCES_DICECTORY = "resources";
+    private static final String INFORMATION_RESOURCE_DIRECTORY_DIRECTORY = RESOURCES_DICECTORY + "/informationresourcedirectories";
     private static final String NETWORKMAPS_DIRECTORY = RESOURCES_DICECTORY + "/networkmaps";
     private static final String COSTMAP_DIRECTORY = RESOURCES_DICECTORY + "/costmaps";
+    private static final String CALENDAR_COSTMAP_DIRECTORY = RESOURCES_DICECTORY + "/calendarcostmaps";
+
+    private List<InformationResourceDirectoryDTO> informationResourceDirectoryDTOList;
+    private List<NetworkMapDTO> networkMapDTOList;
+    private List<CostMapDTO> costMapDTOList;
+    private List<CalendarCostMapDTO> calendarCostMapDTOList;
 
     private static final Logger logger = LoggerFactory.getLogger(BootstrapFromResourcesDirectory.class);
 
+    private InformationResourceDirectoryRepository informationResourceDirectoryRepository;
     private NetworkMapRepository networkMapRepository;
     private CostMapRepository costMapRepository;
+    private CalendarCostMapRepository calendarCostMapRepository;
 
     private ObjectMapper objectMapper;
 
     @Autowired
-    public BootstrapFromResourcesDirectory(NetworkMapRepository networkMapRepository, CostMapRepository costMapRepository, ObjectMapper objectMapper) {
+    public BootstrapFromResourcesDirectory(InformationResourceDirectoryRepository informationResourceDirectoryRepository,
+                                           NetworkMapRepository networkMapRepository,
+                                           CostMapRepository costMapRepository,
+                                           CalendarCostMapRepository calendarCostMapRepository,
+                                           ObjectMapper objectMapper) {
+        this.informationResourceDirectoryRepository = informationResourceDirectoryRepository;
         this.networkMapRepository = networkMapRepository;
         this.costMapRepository = costMapRepository;
+        this.calendarCostMapRepository = calendarCostMapRepository;
         this.objectMapper = objectMapper;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        logger.info("Bootstrapping from resources directory");
+        configureObjectMapper();
+        loadResources();
+        saveResources();
+    }
+
+    private void loadResources() {
+        logger.info("Bootstrapping objects from resources directory");
+
+        logger.info("Loading information resource directories");
+        informationResourceDirectoryDTOList = loadFilesAsObjectsByDepthSearch(INFORMATION_RESOURCE_DIRECTORY_DIRECTORY, InformationResourceDirectoryDTO.class);
 
         logger.info("Loading network maps");
-        List<NetworkMapDTO> networkMapDTOList = loadFilesAsObjectsByDepthSearch(NETWORKMAPS_DIRECTORY, NetworkMapDTO.class);
+        networkMapDTOList = loadFilesAsObjectsByDepthSearch(NETWORKMAPS_DIRECTORY, NetworkMapDTO.class);
 
         logger.info("Loading cost maps");
-        List<CostMapDTO> costMapDTOList = loadFilesAsObjectsByDepthSearch(COSTMAP_DIRECTORY, CostMapDTO.class);
+        costMapDTOList = loadFilesAsObjectsByDepthSearch(COSTMAP_DIRECTORY, CostMapDTO.class);
+
+        logger.info("Loading calendar cost maps");
+        calendarCostMapDTOList = loadFilesAsObjectsByDepthSearch(CALENDAR_COSTMAP_DIRECTORY, CalendarCostMapDTO.class);
+    }
+
+    private void saveResources() {
+        logger.info("Saving information resource directories");
+
+        logger.info("Saving information resource directories");
+        informationResourceDirectoryRepository.insert(informationResourceDirectoryDTOList);
 
         logger.info("Saving network maps");
         networkMapRepository.insert(networkMapDTOList);
 
         logger.info("Saving cost maps");
         costMapRepository.insert(costMapDTOList);
+
+        logger.info("Saving calendar cost maps");
+        calendarCostMapRepository.insert(calendarCostMapDTOList);
+    }
+
+    private void configureObjectMapper() {
+        SimpleModule validationModule = new SimpleModule();
+        validationModule.setDeserializerModifier(new BeanDeserializerModifier() {
+            @Override
+            public JsonDeserializer<?> modifyDeserializer(DeserializationConfig config, BeanDescription beanDesc, JsonDeserializer<?> deserializer) {
+                if (deserializer instanceof BeanDeserializer) {
+                    return new BeanValidationDeserializer((BeanDeserializer) deserializer);
+                }
+                return deserializer;
+            }
+        });
+        objectMapper.registerModule(validationModule);
     }
 
     private <T> List<T> loadFilesAsObjectsByDepthSearch(String baseDirectory, Class<T> objectClass) {
@@ -75,6 +137,7 @@ public class BootstrapFromResourcesDirectory implements CommandLineRunner {
     }
 
     private <T> T loadFileAsObject(File file, Class<T> objectClass) {
+        logger.info(String.format("Loading %s as a %s", file.getName(), objectClass.getName())) ;
         T resource = null;
         try {
             resource = objectMapper.readValue(file, objectClass);
