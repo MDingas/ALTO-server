0d6ff52acae70ea6d1d65c5b67a19db4210dc712
Paulo Caldas <pauloedgar2@gmail.com>
2020-04-16 23:19:02 +0100

    Removed unused network map repositories

--- src/main/java/com/example/restservice/repository/NetworkMapRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.restservice.repository;
-
-import com.example.restservice.dto.networkmap.NetworkMapDTO;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NetworkMapRepository extends MongoRepository<NetworkMapDTO, String>, CustomNetworkMapRepository {
-}
